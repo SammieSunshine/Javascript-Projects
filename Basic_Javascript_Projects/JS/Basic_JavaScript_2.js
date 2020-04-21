afb1c850463b791006990fa06e49e4a3be8a163a
Sam <samanthabillips@gmail.com>
2020-04-21 14:23:42 -0500

    Assignment 60 and 61
    within same HTML and JS files.

---
+++ Basic_Javascript_Projects/JS/Basic_JavaScript_2.js
@@ -0,0 +1,17 @@
+window.alert("This is a Window Alert");
+
+function myFirstfunction() {
+    var e="pinky";
+    var d="promise";
+    var e= e.fontcolor("pink");
+    var d= d.fontcolor("purple");
+        
+        document.getElementById("basic").innerHTML=e+d;
+    }
+
+function SecondFunction() {
+    var a_sentence ="Feeling good ";
+    a_sentence += "and great!";
+    document.getElementById("Hey").innerHTML=a_sentence;
+}
+
