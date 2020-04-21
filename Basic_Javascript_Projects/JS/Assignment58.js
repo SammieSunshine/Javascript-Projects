7bc8f52ac325e78a978c45f55a815d928e12e9a8
Sam <samanthabillips@gmail.com>
2020-04-20 20:07:53 -0500

    JS course project folder

---
+++ Basic_Javascript_Projects/JS/Assignment58.js
@@ -0,0 +1,10 @@
+var a="A window alert";
+window.alert(a);
+
+var d="first str", e="second str";
+var d= d.fontcolor("blue");
+
+document.write(d);
+
+document.write(8+7);
+
---
+++ Basic_Javascript_Projects/JS/Basic_Javascript_1.js
@@ -0,0 +1,40 @@
+var b="This string was created using the variable, b.";
+window.alert(b);
+
+var a="A string, this is!";
+document.write(a);
+
+
+
+document.write("<br><br> She exclaimed, \"Elizabeth, wait! Would it change your mind if I said he was an orthodontist?\ ");
+
+
+document.write("<br><br>This is a concatenate string. "
++ "I wonder if you look both ways when you cross my mind."
++"I\'m sick of chasing. You\'re the one that's always running through my daydream."
++" I can only see your face "
++"when I close my eyes.");
+
+var c=" Currently looking for"+" \'95 Leo."
+document.write(c);
+
+var desert="cake", drink="milk", breakfast="cereal", lunch="burrito", side="side", goodsnack="almonds", badsnack="candy";
+
+var desert= desert.fontcolor("pink");
+
+var drink= drink.fontcolor("teal");
+
+var breakfast= breakfast.fontcolor("purple");
+
+var lunch= lunch.fontcolor("beige");
+
+var side= side.fontcolor("green");
+
+var goodsnack= goodsnack.fontcolor("brown");
+
+var badsnack= badsnack.fontcolor("red");
+
+
+document.write(drink+breakfast);
+
+document.write(7+3);
