b0536c953f3be2d9c344a0477ad5e666bf12a9e9
Sam <samanthabillips@gmail.com>
2020-04-23 15:23:01 -0500

    additional operations

--- Basic_Javascript_Projects/JS/Basic_Javascript_3.js
+++ Basic_Javascript_Projects/JS/Basic_Javascript_3.js
@@ -5,5 +5,26 @@ function addition_function() {
 
 function subtract() {
     var sub= 15-5;
-    document.getElementById("Sub").innerHTML="15 - 5 =" - sub;
+    document.getElementById("Sub").innerHTML="15 - 5 =" + sub;
+}
+
+function multiply() {
+    var Multiply= 10*10;
+    document.getElementById("Mult").innerHTML="10 x 10 ="+ Multiply;
+
+}
+
+function divide() {
+    var slash= 100/2;
+    document.getElementById("Di").innerHTML="100 / 2 =" +slash;
+}
+
+function all_ops() {
+    var all=(10+5) * 100/2 -10;
+    document.getElementById("all").innerHTML= "10 plus 5, multiplied by 100 divided in half, and then subtracted by 10 =" +all;
+}
+
+function modulus(){
+    var modop= 50%9;
+    document.getElementById("mod").innerHTML="When 50 is divided by 9, the remainder would be: " +modop;
 }
