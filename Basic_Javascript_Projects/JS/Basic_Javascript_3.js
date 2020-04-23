01c64674e56e25ccaccefc27123657f519777751
Sam <samanthabillips@gmail.com>
2020-04-23 13:40:17 -0500

    basic JS 3 assignment 71

---
+++ Basic_Javascript_Projects/JS/Basic_Javascript_3.js
@@ -0,0 +1,9 @@
+function addition_function() {
+    var addition= 10+5;
+    document.getElementById("Math").innerHTML="10 + 5 =" + addition;
+}
+
+function subtract() {
+    var sub= 15-5;
+    document.getElementById("Sub").innerHTML="15 - 5 =" - sub;
+}
