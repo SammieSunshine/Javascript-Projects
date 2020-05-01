77e6769d8816201fe06a7878292934f6a21b159a
Sam <samanthabillips@gmail.com>
2020-05-01 16:03:26 -0500

    html/js 9 and css upload

--- Basic_Javascript_Projects/JS/Basic_Javascript_9.js
+++ Basic_Javascript_Projects/JS/Basic_Javascript_9.js
@@ -12,7 +12,7 @@ function countdown() {
         tick();
 }
 
-var slideIndex=1;
+var slideIndex= 1;
 showSlides(slideIndex);
 
 //Next/Prev controls
@@ -30,16 +30,15 @@ function showSlides(n) {
     var slides= document.getElementsByClassName("mySlides");
     var dots= document.getElementsByClassName("dot");
     
-    if (n > slides.length) {slideIndex =1}
-    if (n <1) {slideIndex =slides.length}
-    
+    if (n > slides.length) {slideIndex = 1}
     for (i =0; i < slides.length; i++) {
         slides[i].style.display ="none";
     }
-    for(i=0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace ("active","");
+    if (n < 1) {slideIndex > slides.length}
+    for (i=0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex -1].style.display ="block";
-    dots[slideIndex-1].className += "active";
+    slides[slideIndex-1].style.display ="block";
+    dots[slideIndex-1].classsName += "active";
 }
 
