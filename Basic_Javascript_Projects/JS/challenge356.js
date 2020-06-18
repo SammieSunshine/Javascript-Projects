e8386e770593d65784aab0ff884b07706b5b69cf
Sam <samanthabillips@gmail.com>
2020-06-18 00:27:34 -0500

    challenges 357 to 359
    356: form and form validation
    357:  popup form
    358: keyframe animation
    359: CSS zoom in and out

--- Basic_Javascript_Projects/JS/challenge356.js
+++ Basic_Javascript_Projects/JS/challenge356.js
@@ -1,7 +1,28 @@
+//challenge 356: form/ form validation
+
 function validateForm() {
-    var x = document.forms["valForm"].value;
+    var x = document.forms["valForm"]["fname"].value;
+    var y = document.forms["valForm"]["email"].value;
+    var z = document.forms["valForm"]["pnumber"].value;
     if(x== "") {
         alert("Please enter infomation in all fields");
         return false;
     }
+    if(y== "") {
+        alert("Please enter infomation in all fields");
+        return false;
+    }
+    if(z== "") {
+        alert("Please enter infomation in all fields");
+        return false;
+    }
+}
+
+//challenge 357: popup contact form
+function openForm() {
+    document.getElementById("pop").style.display="block";
+}
+
+function closeForm(){
+    document.getElementById("pop").style.display="none";
 }
